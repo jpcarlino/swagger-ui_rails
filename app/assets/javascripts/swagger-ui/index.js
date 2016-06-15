f6fab1f129bae4ddab5d0e0867654e413dc171c6
Juan Pablo Carlino <jpcarlino@process-one.net>
2016-06-15 17:59:34 -0300

    Update to jQuery 1.11.0
    This also requires to update jQuery BBQ plugin. Here i use the forked
    version from https://github.com/eosite/jquery-bbq

--- app/assets/javascripts/swagger-ui/index.js
+++ app/assets/javascripts/swagger-ui/index.js
@@ -1,8 +1,8 @@
 //= require ./lib/shred.bundle.js
-//= require ./lib/jquery-1.8.0.min.js
+//= require ./lib/jquery-1.11.0.min.js
 //= require ./lib/jquery.slideto.min.js
 //= require ./lib/jquery.wiggle.min.js
-//= require ./lib/jquery.ba-bbq.min.js
+//= require ./lib/jquery.ba-bbq.eosite.a728beef.min.js
 //= require ./lib/handlebars-1.0.0.js
 //= require ./lib/underscore-min.js
 //= require ./lib/backbone-min.js
@@ -10,4 +10,3 @@
 //= require ./lib/swagger-client.js
 //= require ./swagger-ui.js
 //= require ./lib/highlight.7.3.pack.js
-
